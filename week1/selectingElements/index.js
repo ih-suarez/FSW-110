5876d301c5deab3caf15ff66af5ed93af0dc0371
Ismael Suarez <spawn@Ismaels-MacBook-Pro.local>
2020-08-01 14:49:14 -0500

    selectingElements

--- week1/selectingElements/index.js
+++ week1/selectingElements/index.js
@@ -1,16 +1,24 @@
+// Document
+
+console.log(document);
+
 // Practice
+
 var sElement = document.getElementById('sElement');
     console.log(sElement);
     console.dir(sElement);
+    
 
-// Selecting Elements
+// Selecting Elements Id
 
 let h1Element = document.getElementById('sElement');
     h1Element.innerHTML += 'This is Selecting Elements';
+    console.log('Selecting Elements By Id')
 
 // Append  Child
 
 let appElement = document.createElement('h1');
     appElement.id = 'sElement';
-    appElement.textContent = 'Append Child';
+    appElement.textContent = 'Appending Child';
     document.body.appendChild(appElement);
+    console.log('Appending Child') 

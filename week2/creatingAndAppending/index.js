91912b63ec03b7c69dfb36c353756f247740468c
Ismael Suarez <spawn@Ismaels-MacBook-Pro.local>
2020-08-08 21:19:41 -0500

    creatingAndAppending

---
+++ week2/creatingAndAppending/index.js
@@ -0,0 +1,37 @@
+
+// Header 
+
+
+var siteHeader = document.createElement('h1');
+siteHeader.textContent = 'Welcome to my JS site';
+document.body.append(siteHeader);
+
+
+// Paragraph
+
+
+var sitePa = document.createElement('p');
+sitePa.textContent = 'All this was created with Javascript';
+document.body.append(sitePa);
+
+
+// Ordered list
+
+
+var siteOl = document.createElement('ol');
+siteOl.textContent = 'List'
+document.body.append(siteOl);
+
+var li1 = document.createElement('li');
+li1.textContent = 'Hello';
+
+var li2 = document.createElement('li');
+li2.textContent = 'Big';
+
+var li3 = document.createElement('li');
+li3.textContent = 'World';
+
+
+document.body.append(li1);
+document.body.append(li2);
+document.body.append(li3);
